dba7f55986463ddedb385ef56df78202caedbb2c
Haribabu480 <harigudi480@gmail.com>
2025-09-08 10:56:27 +0530

    helloworld message

---
+++ Helloworld.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send("hello world");
+});
+const port=2000;
+app.listen(port,()=>{
+    console.log(`server runned at http://localhost:${port}`);
+})
