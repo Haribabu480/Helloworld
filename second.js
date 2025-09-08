7a07e35e4755e5869234d102f0f80ce6d2c9833f
Haribabu480 <harigudi480@gmail.com>
2025-09-08 12:46:13 +0530

    second file

---
+++ second.js
@@ -0,0 +1,13 @@
+const express=require('express');
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send("second hello world program");
+})
+
+const port=3000;
+
+app.listen(port,()=>{
+    console.log(`server created at http://localhost:${port}`);
+})
